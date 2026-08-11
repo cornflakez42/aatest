ea19d5cbb039f26c01835f1b9e53e1b2d7174479
cornflakez42 <charllesmcadam@gmail.com>
2026-08-11 18:14:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello from Android Termux Build!")
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello from Android App!")
+
+if __name__ == '__main__':
+    MyApp().run()
